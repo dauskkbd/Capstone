3863a20ac082d1451cd3b23c0b1e3ebdc7da8767
dauskkbd <dauskkbd@gmail.com>
2024-02-05 10:38:03 +0800

    backing up files for main Capstone repository

--- RollNPlay/resources/views/shop.blade.php
+++ RollNPlay/resources/views/shop.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @include('layouts/head_section')
+    @include('layouts/head')
     <title>RollNPlay-</title>
 </head>
 <body>
     @include('layouts/navbar')
+    
     @include('layouts/footer')
 </body>
 </html>
--- RollNPlay/routes/web.php
+++ RollNPlay/routes/web.php
@@ -2,17 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('home');
+Route::get("/test", function () {
+    return view("about");
+});
+
+Route::get("/login", function () {
+    return view("login");
+});
+
+Route::get("/register", function () {
+    return view("register");
+});
+
+Route::get("/home", function () {
+    return view("home");
+});
+
+Route::get("/about", function () {
+    return view("about");
+});
+
+Route::get("/dashboard", function () {
+    return view("dashboard");
 });
